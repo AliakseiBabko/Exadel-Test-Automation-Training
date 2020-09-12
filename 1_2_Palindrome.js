0b3d29af09c8eed650247b4b021d0062b43493ab
Aliaksei Babko <alexeibobko@gmail.com>
2020-09-12 10:12:11 +0300

    add solution to the second task of the first homework

--- 1_2_Palindrome.js
+++ 1_2_Palindrome.js
@@ -1,9 +1,9 @@
 function Palindrome (word) {
     const length = word.length;
     for (let i = 0; i <= length/2; i++) {
-        if (word.charAt(i) != word.charAt(length-i-1)) {return console.log('The word is not a palindrome')};
+        if (word.charAt(i) != word.charAt(length-i-1)) {return console.log('Not a palindrome')};
     }
-    return console.log('The word is a palindrome');
+    return console.log('It is a palindrome!');
 }
 
 Palindrome ('Step on no pets');
--- 1_3_Replacement.js
+++ 1_3_Replacement.js
@@ -1,3 +1,24 @@
-function Replacement (word, position, character) {
-    a
+function Replacement (text, position, character) {
+    let newText = text; //create a copy of the text to work with
+    let currentPosition = 0; //a marker to indicate the current position in a word  
+    const textLength = newText.length; //to prevent counting text length each time
+    for (let i = 0; i <= textLength; i++) {
+        if (newText.charAt(i) == ' ') {
+            currentPosition = 0;  //reset counter when we reached a space
+        } else if (currentPosition == position - 1) {
+            newText = replaceChar (newText, i, character); //replace character in the given position 
+            currentPosition++;
+        } else {
+            currentPosition++; //
+        };        
+    }
+    console.log(newText);
 }
+
+function ReplaceChar (newText, i, character) {
+    let firstPart = newText.substr(0, i);
+    let lastPart = newText.substr(i + 1);
+    return firstPart + character + lastPart;
+}
+
+Replacement ('Top aide to a Durham steps down amid Russia origins probe', 2, '0');
