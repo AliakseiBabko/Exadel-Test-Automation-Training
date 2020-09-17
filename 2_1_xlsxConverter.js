bcf3b338e76d9425eb625509774d7c62ea2e2fd4
Aliaksei Babko <alexeibobko@gmail.com>
2020-09-18 01:32:07 +0300

    add initial not working solution to the first task of the second homework

--- 2_1_xlsxConverter.js
+++ 2_1_xlsxConverter.js
@@ -1,1 +1,12 @@
 fs = require('fs');
+const json2xls = require('./node_modules/json2xls/lib/json2xls.js');
+
+fs.readFile('JSON_to_xlsx.json', 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
+    }
+    const xls = json2xls(data);
+    fs.writeFileSync('data.xlsx', xls, 'binary', function (err) {
+        if (err) throw err;
+    });
+})
