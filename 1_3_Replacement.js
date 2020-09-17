c3863f2b02fab1a981347cb8cd69bfde62f88c6f
Aliaksei Babko <alexeibobko@gmail.com>
2020-09-17 15:09:15 +0300

    code refactoring for the second task of the first homework

--- 1_3_Replacement.js
+++ 1_3_Replacement.js
@@ -1,24 +1,30 @@
-function Replacement (text, position, character) {
+function Replacement (text, positionToReplace, insert) {
+    const regex = /[ `!@#$%^&*()_+\-=\[\]{};'’:"\\|,.<>\/?~]/; //list of special characters
     let newText = text; //create a copy of the text to work with
-    let currentPosition = 0; //a marker to indicate the current position in a word  
-    const textLength = newText.length; //to prevent counting text length each time
-    for (let i = 0; i <= textLength; i++) {
-        if (newText.charAt(i) === ' ') {
-            currentPosition = 0;  //reset counter when we reached a space
-        } else if (currentPosition === position - 1) {
-            newText = replaceChar (newText, i, character); //replace character in the given position 
-            currentPosition++;
+    let positionInWord = 0; //a counter for the current position in a word
+    let positionInText = 0; //a counter for the current position in the text
+    for (let i of text) { //loop through the original text
+        if (i === ' ') { //chech if there is a space in current position (word separator)
+            positionInWord = 0;  //reset position in the word
+            positionInText++;
+        } else if (positionInWord === positionToReplace - 1 && !i.match(regex)) { //check if we reach positionToReplace and it's not a special character
+            newText = replaceChar (newText, positionInText, insert); //if yes, replace character in the given position 
+            positionInWord += insert.length;
+            positionInText += insert.length;
+        } else if (i.match(regex)) { //check if current character is a special one
+            positionInText++; //if yes, we don't count it within the current word
         } else {
-            currentPosition++; //
-        };        
+            positionInWord++; //if it's not a special character and not a positionToReplace, we just proceed
+            positionInText++; 
+        };
     }
     console.log(newText);
 }
 
-function replaceChar (newText, i, character) {
-    let firstPart = newText.substr(0, i);
-    let lastPart = newText.substr(i + 1);
-    return firstPart + character + lastPart;
+function replaceChar (newText, positionInText, insert) {
+    let firstPart = newText.substr(0, positionInText);
+    let lastPart = newText.substr(positionInText + 1);
+    return firstPart + insert + lastPart;
 }
 
-Replacement ('Top aide to a Durham steps down amid Russia origins probe', 2, '0');
+Replacement ("It’s a disaster, he told Carlson. We've already seen the near obstruction of journalism.", 3, '000');
